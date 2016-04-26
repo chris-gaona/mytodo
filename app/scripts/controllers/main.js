38269497b034e58e6705c0c0fa3ec031c355fe22
Chris Gaona <thebeloved88@sbcglobal.net>
2016-04-26 11:00:38 -0700

    Adds functionality to add new task

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,5 +9,10 @@
  */
 angular.module('projectsApp')
   .controller('MainCtrl', function () {
-    this.todos = ['Item 1', 'Item 2', 'Item 3'];
+    this.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+
+    this.addTodo = function(todo) {
+      this.todos.push(todo);
+      this.todo = '';
+    };
   });
